a1ca2b41c7669ca7b6bfb8d20c72dee8094166c8
Sriharsha <sriharsha_sureshbabu@epam.com>
2023-06-21 10:18:13 +0530

    fixes

--- src/NewExcel.java
+++ src/NewExcel.java
@@ -4,6 +4,8 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class NewExcel {
     public static void main(String[] args) {
@@ -17,11 +19,11 @@ public class NewExcel {
         String[] headers = {"project", "Test Case", "Type", "Parent", "Assignee", "priority", "labels", "seal ld", "description"};
 
         // Create the header row
-        Row headerRow = sheet.createRow(0);
+        Row headerRow_ = sheet.createRow(0);
 
         // Write headers into cells
         for (int i = 0; i < headers.length; i++) {
-            Cell cell = headerRow.createCell(i);
+            Cell cell = headerRow_.createCell(i);
             cell.setCellValue(headers[i]);
         }
 
@@ -38,24 +40,91 @@ public class NewExcel {
             StringBuilder stepsString = new StringBuilder();
             stepsString.append("||Steps||Description||Expected Result||\n");
 
+            // Find the header row in the input sheet
+            Row headerRow = inputSheet.getRow(0);
 
-            // Iterate over the rows in the input sheet
+            // Find the column indices based on header matching
+            int projectColumnIndex = -1;
+            int testCaseColumnIndex = -1;
+            int testTypeColumnIndex = -1;
+            int testParentColumnIndex = -1;
+            int testAssigneeColumnIndex = -1;
+            int testPriorityColumnIndex = -1;
+            int testLabelsColumnIndex = -1;
+            int testSealidColumnIndex = -1;
+            int testStepsColumnIndex = -1;
+            int testDescColumnIndex = -1;
+            int testExpectedColumnIndex = -1;
+
+            for (Cell headerCell : headerRow) {
+                String headerCellValue = headerCell.getStringCellValue().toLowerCase();
+
+                switch (headerCellValue.toLowerCase()) {
+                    case "project":
+                        projectColumnIndex = headerCell.getColumnIndex();
+                        break;
+                    case "testcase":
+                        testCaseColumnIndex = headerCell.getColumnIndex();
+                        break;
+                    case "type":
+                        testTypeColumnIndex = headerCell.getColumnIndex();
+                        break;
+                    case "parent":
+                        testParentColumnIndex = headerCell.getColumnIndex();
+                        break;
+                    case "asignee":
+                        testAssigneeColumnIndex = headerCell.getColumnIndex();
+                        break;
+                    case "priority":
+                        testPriorityColumnIndex = headerCell.getColumnIndex();
+                        break;
+                    case "label":
+                        testLabelsColumnIndex = headerCell.getColumnIndex();
+                        break;
+                    case "sealid":
+                        testSealidColumnIndex = headerCell.getColumnIndex();
+                        break;
+                    case "step name":
+                        testStepsColumnIndex = headerCell.getColumnIndex();
+                        break;
+                    case "step description":
+                        testDescColumnIndex = headerCell.getColumnIndex();
+                        break;
+                    case "expected result":
+                        testExpectedColumnIndex = headerCell.getColumnIndex();
+                        break;
+                }
+            }
+
+            if (projectColumnIndex == -1 || testCaseColumnIndex == -1 || testTypeColumnIndex == -1 ||
+                    testParentColumnIndex == -1 || testAssigneeColumnIndex == -1 ||
+                    testPriorityColumnIndex == -1 || testLabelsColumnIndex == -1 ||
+                    testSealidColumnIndex == -1 || testStepsColumnIndex == -1 ||
+                    testDescColumnIndex == -1 || testExpectedColumnIndex == -1) {
+                System.err.println("One or more headers not found in the input sheet.");
+                return; // or handle the error as per your requirement
+            }
+            StringBuilder subStepString = new StringBuilder();
+            List<StringBuilder> stepList = new ArrayList<>();
             for (int i = 1; i <= inputSheet.getLastRowNum(); i++) {
                 Row inputRow = inputSheet.getRow(i);
+
+
                 Row newRow = sheet.createRow(i + 1);  // Create the row only once
 
-                // Read the value from the 4th column (index 3) in the input sheet
-                Cell inputValueCell = inputRow.getCell(4);
-                if (inputValueCell != null && inputValueCell.getCellType() == CellType.STRING) {
-                    String projectValue = inputValueCell.getStringCellValue();
+                // Read the values based on the column indices
+                // project
+                Cell projectCell = inputRow.getCell(projectColumnIndex);
+                if (projectCell != null && projectCell.getCellType() == CellType.STRING) {
+                    String projectValue = projectCell.getStringCellValue();
 
                     // Write the value to the corresponding column in the new sheet
                     Cell outputValueCell = newRow.createCell(0);
                     outputValueCell.setCellValue(projectValue);
                 }
 
-                // Read the value from the 1st column (index 0) in the input sheet
-                Cell testCaseCell = inputRow.getCell(0);
+                // testcase
+                Cell testCaseCell = inputRow.getCell(testCaseColumnIndex);
                 if (testCaseCell != null && testCaseCell.getCellType() == CellType.STRING) {
                     String testCaseValue = testCaseCell.getStringCellValue();
 
@@ -65,7 +134,7 @@ public class NewExcel {
                 }
 
                 // type
-                Cell testTypeCell = inputRow.getCell(5);
+                Cell testTypeCell = inputRow.getCell(testTypeColumnIndex);
                 if (testTypeCell != null && testTypeCell.getCellType() == CellType.STRING) {
                     String testCaseValue = testTypeCell.getStringCellValue();
 
@@ -75,7 +144,7 @@ public class NewExcel {
                 }
 
                 // parent
-                Cell testParentCell = inputRow.getCell(6);
+                Cell testParentCell = inputRow.getCell(testParentColumnIndex);
                 if (testParentCell != null && testParentCell.getCellType() == CellType.STRING) {
                     String testCaseValue = testParentCell.getStringCellValue();
 
@@ -85,7 +154,7 @@ public class NewExcel {
                 }
 
                 // assignee
-                Cell testAssigneeCell = inputRow.getCell(7);
+                Cell testAssigneeCell = inputRow.getCell(testAssigneeColumnIndex);
                 if (testAssigneeCell != null && testAssigneeCell.getCellType() == CellType.STRING) {
                     String testCaseValue = testAssigneeCell.getStringCellValue();
 
@@ -95,7 +164,7 @@ public class NewExcel {
                 }
 
                 // priority
-                Cell testPriorityCell = inputRow.getCell(8);
+                Cell testPriorityCell = inputRow.getCell(testPriorityColumnIndex);
                 if (testPriorityCell != null && testPriorityCell.getCellType() == CellType.STRING) {
                     String testCaseValue = testPriorityCell.getStringCellValue();
 
@@ -105,10 +174,9 @@ public class NewExcel {
                 }
 
                 // labels
-                Cell testlabelsCell = inputRow.getCell(9);
-                if (testlabelsCell != null && testlabelsCell.getCellType() == CellType.STRING) {
-                    String testCaseValue = testlabelsCell.getStringCellValue();
-
+                Cell testLabelsCell = inputRow.getCell(testLabelsColumnIndex);
+                if (testLabelsCell != null && testLabelsCell.getCellType() == CellType.STRING) {
+                    String testCaseValue = testLabelsCell.getStringCellValue();
 
                     // Write the value to the corresponding column in the new sheet
                     Cell outputValueCell = newRow.createCell(6);
@@ -116,40 +184,52 @@ public class NewExcel {
                 }
 
                 // sealid
-                Cell testSealidCell = inputRow.getCell(10);
+                Cell testSealidCell = inputRow.getCell(testSealidColumnIndex);
                 if (testSealidCell != null && testSealidCell.getCellType() == CellType.NUMERIC) {
-                    Number testCaseValue = testSealidCell.getNumericCellValue();
+                    double testCaseValue = testSealidCell.getNumericCellValue();
 
                     // Write the value to the corresponding column in the new sheet
                     Cell outputValueCell = newRow.createCell(7);
-                    outputValueCell.setCellValue((Double) testCaseValue);
+                    outputValueCell.setCellValue(testCaseValue);
                 }
 
-
-                // description
-                Cell testStepsCell = inputRow.getCell(1);
-                Cell  testDescCell= inputRow.getCell(2);
-                Cell testExoectedCell = inputRow.getCell(3);
-                if (
-                        testStepsCell != null && testStepsCell.getCellType() == CellType.STRING &&
+                // steps, description, expected result
+                Cell testStepsCell = inputRow.getCell(testStepsColumnIndex);
+                Cell testDescCell = inputRow.getCell(testDescColumnIndex);
+                Cell testExpectedCell = inputRow.getCell(testExpectedColumnIndex);
+                if (testStepsCell != null && testStepsCell.getCellType() == CellType.STRING &&
                         testDescCell != null && testDescCell.getCellType() == CellType.STRING &&
-                        testExoectedCell != null && testExoectedCell.getCellType() == CellType.STRING
-                ) {
-
-
-                    stepsString.append(testStepsCell.getStringCellValue())
+                        testExpectedCell != null && testExpectedCell.getCellType() == CellType.STRING) {
+                    if (testCaseCell != null) {
+                        stepList.add(subStepString);
+                        subStepString = new StringBuilder();
+                    }
+                    subStepString.append(testStepsCell.getStringCellValue())
                             .append("|")
                             .append(testDescCell.getStringCellValue())
                             .append("|")
-                            .append(testExoectedCell.getStringCellValue())
+                            .append(testExpectedCell.getStringCellValue())
                             .append("\n");
-
-                    Cell outputValueCell = newRow.createCell(8);
-                    outputValueCell.setCellValue(stepsString.toString());
                 }
             }
-
-
+            if (!subStepString.toString().equals("")) {
+                stepList.add(stepsString.append(subStepString));
+            }
+            for (int i = 1; i < sheet.getLastRowNum(); i++) {
+                Row sheetRow = sheet.getRow(i);
+                if (sheetRow != null && sheetRow.getCell(1) != null) {
+                    for (int j = 1; j < stepList.size(); j++) {
+                        Cell outputValueCell = sheetRow.createCell(8);
+                        outputValueCell.setCellValue(stepList.get(j).toString());
+                    }
+                }
+                else{
+                    if(sheetRow != null) {
+                        sheet.removeRow(sheetRow);
+                    }
+                }
+            }
+            // Iterate over the rows in the input sheet
         } catch (IOException e) {
             System.err.println("Error reading Excel file: " + e.getMessage());
         }
